f0539359afec61c8083e40b475962e6557afa6a9
tran986 <tran.986@buckeyemail.osu.edu>
2025-09-05 16:47:16 -0400

    fix gzipping

--- pipeline_project.py
+++ pipeline_project.py
@@ -12,7 +12,8 @@ from glob import glob
 #------------------------------------------------S1:DOWNLOAD RAW SEQ. SAMPLES FROM ENA: PRJEB7774
 #***Gut microbiome development along the colorectal adenoma-carcinoma sequence (Feng et al, 2015)
 
-seq_info = pd.read_csv("/mnt/c/Users/Nia Tran/colon_cancer_metaG/CRC_seq_info.txt", sep="\t")
+seq_info = pd.read_csv("/Users/Nghitran/colon_cancer_metaG/CRC_seq_info.txt", sep="\t")
+#seq_info = pd.read_csv("/mnt/c/Users/Nia Tran/colon_cancer_metaG/CRC_seq_info.txt", sep="\t")
 
 #access sample ID from metadata:
 print(seq_info.head(5))
@@ -22,7 +23,7 @@ print(seq_info.columns)
 seq_single=seq_info[seq_info['submitted_ftp'].str.contains('clean.single')]
 print(seq_single)
 
-#Randomly choose 5 controls, 5 advanced adenoma, 5 carcinoma samples:
+#Randomly choose 1 controls, 1 advanced adenoma, 5 carcinoma samples:
 
 c = ["Stool sample from controls",
      "Stool sample from advanced adenoma",
@@ -31,7 +32,7 @@ c = ["Stool sample from controls",
 combined_url=[]
 for i in c:
     fastq_ftp = seq_single[seq_single["sample_title"] == i]["fastq_ftp"]
-    all_url=random.sample(fastq_ftp.tolist(), 5) #changes this if need more/less samples
+    all_url=random.sample(fastq_ftp.tolist(), 1) #changes this if need more/less samples
     combined_url.extend(all_url)
 
 # Directory to save files - Dowloads
@@ -44,7 +45,7 @@ for url in combined_url:
     url = "https://" + url
     filepath = os.path.join(out_dir, filename)
     print(f"Downloading {url} -> {filepath}")
-    #urllib.request.urlretrieve(url, filepath)
+    #urllib.request.urlretrieve(url, filepath)    #download fastq seq
 
 #make a metadata for downstream analysis later on:
 metadata_samp = seq_info[seq_info["fastq_ftp"].isin(combined_url)][["run_accession","sample_title"]]
@@ -72,20 +73,23 @@ os.makedirs(qc_dir, exist_ok=True)
 
 for filename in os.listdir(out_dir):
    filepath=os.path.join(out_dir, filename)
-   print(f"running QC on {filename} to {filepath}")
+   print(f"running QC on {filename} to {filepath}") #run fastqc
    """
-   subprocess.run(
+   subprocess.run(                             
    ["fastqc", filepath, "-o", qc_dir]
    )
    """
 
 #--------MULTIQC:
 multiqc_dir = "multiqc_dir"
-#multiqc.run(qc_dir, "-o", multiqc_dir)
+#multiqc.run(qc_dir, "-o", multiqc_dir)            #run multiqc
 
-#remove fastqc dir after multiqc results are generated:
+#remove fastqc/multiqc dir after multiqc results are generated:
 #shutil.rmtree(qc_dir)
 print(f"{qc_dir} has been removed to clear disk space")
+#shutil.rmtree(multiqc_dir)
+print(f"{multiqc_dir} has been remove to clear disk space")
+
 
 #--------FASTP for trimming
 trimmed_dir = "fastp_dir"
@@ -95,28 +99,28 @@ os.makedirs(trimmed_dir, exist_ok=True)
 for filename in os.listdir(out_dir):
    filepath_in=os.path.join(out_dir, filename)
    filepath_out=os.path.join(trimmed_dir, filename)
-   print(f"trimming {filepath_in} to {filepath_out}")
+   print(f"trimming {filepath_in} to {filepath_out}")   #run fastp
    """
-   subprocess.run([
+    subprocess.run([
     "fastp",
     "-i", filepath_in,
     "-o", filepath_out,
     "--html", filepath_out + ".html"])
    """
-   
+
+
 #------------------------------------------------S3 RUN METAPHLAN 4 FOR TAXA PROFILING :
 #splitting into chunks:-in order to run in local machine with low RAM:
-
 tmp_dir="fastq_chunks"
 
 os.makedirs(tmp_dir, exist_ok=True)
 
 lines_per_chunk = 4_000_000  
 nproc = 1
-
+"""
 print("Splitting FASTQ into smaller chunks...")
 for filename in os.listdir(trimmed_dir):
-    if filename.endswith(".gz"):
+    if filename.endswith(".fastq.gz"):
         filepath = os.path.join(trimmed_dir, filename)
         sample_name=filename.split(".")[0]
         with gzip.open(filepath, "rt") as f: #open fastq file in txt mode
@@ -138,7 +142,7 @@ for filename in os.listdir(trimmed_dir):
                         
         print(f"Created {chunk_idx + 1} chunks for {sample_name}.")
 
-#make sure they all gunzipped (insanity check):
+print("Make sure they all gunzipped (insanity check):")
 
 for filename in os.listdir(tmp_dir):
     filepath = os.path.join(tmp_dir, filename)
@@ -149,15 +153,21 @@ for filename in os.listdir(tmp_dir):
     # Check if file is already gzipped
     with open(filepath, 'rb') as f:
         magic_number = f.read(2)
+
     if magic_number != b'\x1f\x8b':  # not gzipped
-        gz_path = filepath + ".gz"
-        with open(filepath, 'rb') as f_in, gzip.open(gz_path, 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
-        os.remove(filepath)  # remove the original uncompressed file
-        print(f"Gzipped {filename} → {os.path.basename(gz_path)}")
+        if filepath.endswith(".fastq.gz"):
+            new_path = filepath[:-3]  # remove .gz extension
+            os.rename(filepath, new_path)       # rename all to unzipped
+            filepath_gz=str(new_path) + ".gz"
+            print(new_path)
+            print(filepath_gz)
+            with open(new_path, 'rb') as f_in, gzip.open(filepath_gz, 'wb') as f_out:
+               f_out.writelines(f_in)
+            print(f"Gzipped {filename}")
     else:
         print(f"Already gzipped: {filename}")
 
+"""
 #run "rm *.fastq" if there are replicates:
 for filename in os.listdir(tmp_dir):
     if filename.endswith(".fastq"):
@@ -165,34 +175,32 @@ for filename in os.listdir(tmp_dir):
         os.remove(filepath)
         print(f"Delete {filename} because of unzipp")
 
-
 #run Metaphlan3:
 taxa_prof_dir = "metaphlan_dir"
-
 os.makedirs(taxa_prof_dir, exist_ok=True)
 
-#metaphlan version 3
+#metaphlan version 3 (compatible with python 3.9)
 #metaphlan --install --bowtie2db "/home/tran986/mp_database_new"
 
 for filename in os.listdir(tmp_dir):
    if filename.endswith(".fastq.gz"):
    #if filename == "ERR710415.fastq.gz" : #test with 1 sample
       input_path=os.path.join(tmp_dir, filename)
-      output_txt=filename.replace(".fastq","_profile.txt")
+      output_txt=filename.replace(".fastq.gz","_profile.txt")
       output_path=os.path.join(taxa_prof_dir,output_txt)
       print(f"profilling taxa in chunk {input_path} to {output_path}")
       subprocess.run(["metaphlan",
                       input_path,
                       "--input_type", "fastq", 
                       "--nproc", "1",
-                      "--bowtie2db", "/home/tran986/mp_database_new",
+                      "--bowtie2db", "/Users/Nghitran/colon_cancer_metaG/metaphlan_database", #"--bowtie2db "/home/tran986/mp_database_new"
                       "--index", "mpa_v31_CHOCOPhlAn_201901",
                       "-o", output_path  
                       ], check=True) #change dir to the db
-
-#merging metaphlan output tbls:
+      
 """
 #need to merge chunk before merge metaphlan + test them out
+#merging metaphlan output tbls:
 merge_metaphlan = os.path.join(taxa_prof_dir, "merged_file.txt")
 subprocess.run([
     "merge_metaphlan_tables.py",
@@ -200,5 +208,3 @@ subprocess.run([
     "--o", merge_metaphlan
 ], check=True)
 """
-
-
